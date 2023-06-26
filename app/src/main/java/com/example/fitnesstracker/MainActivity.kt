76f51030caa5e38797efa3833c7e7c1c607534f5
Kássio Silva <Kassio.Sa@ibm.com>
2023-06-26 15:25:08 -0300

    feat: Criando tela de formulário do imc

--- app/src/main/java/com/example/fitnesstracker/MainActivity.kt
+++ app/src/main/java/com/example/fitnesstracker/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.fitnesstracker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var buttonImc: LinearLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonImc = findViewById(R.id.button_imc)
+
+        buttonImc.setOnClickListener {
+            val intent = Intent(this, ImcActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
