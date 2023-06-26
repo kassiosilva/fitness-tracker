3a3cf43fa6d52b2692d336a5a1103c7e6935c68a
Kássio Silva <Kassio.Sa@ibm.com>
2023-06-26 19:11:33 -0300

    feat: Add verificações que gera o resultado do IMC

--- app/src/main/java/com/example/fitnesstracker/ImcActivity.kt
+++ app/src/main/java/com/example/fitnesstracker/ImcActivity.kt
@@ -2,9 +2,11 @@ package com.example.fitnesstracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.annotation.StringRes
 
 class ImcActivity : AppCompatActivity() {
     private lateinit var editWeight: EditText
@@ -23,12 +25,39 @@ class ImcActivity : AppCompatActivity() {
                 Toast.makeText(this, R.string.fields_message, Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
+
+            val weight = editWeight.text.toString().toInt()
+            val height = editHeight.text.toString().toInt()
+
+            val resultImc = calculateImc(weight, height)
+
+            val imcResponseId = imcResponse(resultImc)
+            Toast.makeText(this, imcResponseId, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun calculateImc(weight: Int, height: Int): Double {
+        return weight / ((height / 100.0) * (height / 100.0))
+    }
+
+    @StringRes
+    private fun imcResponse(imc: Double): Int {
+        return when {
+            imc < 15.0 -> R.string.imc_severely_low_weight
+            imc < 16.0 -> R.string.imc_very_low_weight
+            imc < 18.5 -> R.string.imc_low_weight
+            imc < 25.0 -> R.string.normal
+            imc < 30.0 -> R.string.imc_high_weight
+            imc < 35.0 -> R.string.imc_so_high_weight
+            imc < 40.0 -> R.string.imc_severely_high_weight
+            else -> R.string.imc_extreme_weight
         }
     }
 
     private fun validate(): Boolean {
         val hasValueFields =
             editWeight.text.toString().isNotEmpty() && editHeight.text.toString().isNotEmpty()
+
         val hasBeginningZero =
             !editWeight.text.toString().startsWith("0") && !editHeight.text.toString()
                 .startsWith("0")
